99d9965ced0a77ce1be92635120cfc6a0578411a
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2023-06-01 01:08:04 -0500

    Init menu

--- src/main/java/icesi/security/Main.java
+++ src/main/java/icesi/security/Main.java
@@ -4,6 +4,22 @@ package icesi.security;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        try {
+            menu();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void menu() throws Exception {
+
+        System.out.println("1. Generar llave pública y privada.");
+        System.out.println("2. Firmar documento.");
+        System.out.println("3. Verificar firma del documento.");
+
+        Security security = new Security();
+
+
     }
 }
